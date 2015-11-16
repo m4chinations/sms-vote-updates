3a1073daa700d2e2a9a70e26e25c3b6d6b134002
Tennyson Holloway <tennyson.holloway@gmail.com>
2015-11-16 17:19:16 -0500

    configurable sms endpoint; comments

--- config-sample.js
+++ config-sample.js
@@ -1,25 +1,31 @@
 var config = {};
 
-config.maria = {};
-config.twilio = {};
-config.sunlight = {};
-config.web = {};
+config.maria = {};    /* https://mariadb.org/ */
+config.twilio = {};   /* https://www.twilio.com/ */
+config.sunlight = {}; /* https://sunlightfoundation.com/ */
+config.web = {};      /* web port for express */
 
-config.DEBUG = true;
+config.DEBUG = true;  /* if TRUE no texts will be sent, just logged to console */
 
 config.maria.host = 'xxx.xxx.xxx.xxx';
 config.maria.user = 'user';
 config.maria.pass = 'pw';
 config.maria.db   = 'db';
 
-config.twilio.account_sid = '';
+config.twilio.account_sid = ''; /* see https://twilio.github.io/twilio-node/twilio-credentials.gif */
 config.twilio.auth_token = '';
-config.twilio.number = '+14808675309';
+config.twilio.number = '+14808675309'; /* YOUR TWILIO NUMBER */
+
+/* Your TWILIO ENDPOINT. This is to receive SMSs.
+ * by default this app will listen on /sms for incoming SMS.
+ * so in your twilio config you should have your full URL. e.g.
+ * https://my-heroku-app-name.heroku.com/sms */
 config.twilio.endpoint = 'https://';
 
-config.sunlight.api_key = '';
+config.sunlight.api_key = ''; /* sunlight API key */
 
 
 config.web.port = process.env.PORT || 3000;
+config.web.endpoint = '/sms'; /*where express will listen for incoming SMS */
 
 module.exports = config;
--- index.js
+++ index.js
@@ -6,13 +6,16 @@ var twilio     = require('twilio'),
     bodyParser = require('body-parser'),
     config     = require('./config');
 
+/* initalize twilio with creds and our number */
 var sms = twilio(config.twilio.account_sid, config.twilio.auth_token);
 var myNumber = config.twilio.number;
 
+/* initialize express with some bodyParser magic */
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+/* create the db object with our db credentials */
 var db = new maria({
     host: config.maria.host,
     user: config.maria.user,
@@ -22,6 +25,7 @@ var db = new maria({
 
 var sunlightAPIKey = config.sunlight.api_key;
 
+/* sunlight congress v3 API endpoints */
 var sunlightBill =
     replaceStr('https://congress.api.sunlightfoundation.com/votes?vote_type__in=passage&fields=vote_type,roll_id,bill,voted_at,voter_ids,roll_id,result,breakdown.party&order=voted_at&apikey={{apikey}}',
         { apikey : sunlightAPIKey });
@@ -32,6 +36,7 @@ var sunlightLegislator =
     replaceStr('https://congress.api.sunlightfoundation.com/legislators?bioguide_id={{bid}}&apikey={{apikey}}',
         { apikey : sunlightAPIKey });
 
+/* strings */
 var usageStr = "TXT your ZIPCODE to see available legislators. TXT STOP anytime to STOP. TXT CLEAR to clear subscriptions.";
 var legislatorChoiceStr = "TXT Choice number to select your legislator:";
 var legislatorChoiceOptionStr = "{{n}}. {{name}}";
@@ -41,36 +46,46 @@ var noVoteStr = "{{name}} chose not to vote on {{bill}}. REPLY ? to see breakdow
 var breakdownStr = 'R: YEA {{ry}} NAY {{rn}} NV {{rnv}} D: YEA {{dy}} NAY {{dn}} NV {{dnv}} I: YEA {{iy}} NAY {{in}} NV {{inv}}';
 var unsubscribeStr = 'You have cleared your subscriptions.';
 
+/* sql statements */
 var subscribeUserSQL = "INSERT INTO subscriptions (bioguide_id, number) VALUES (:bid, :number)";
 var getSubscribersSQL = "SELECT number FROM subscriptions WHERE bioguide_id = :bid";
 var unsubscribeSQL = "DELETE FROM subscriptions WHERE number = :number";
 
 var nameCache = {}; //cache bid -> name party-state
 var userChoice = {}; //storage for user prompt.
-var userBreakdown = {};
+var userBreakdown = {}; //storage for user breakdown reply
 
 //DEBUG will direct all SMS to go to console.log instead of twilio
 var DEBUG = config.DEBUG;
 
-var lastVote = moment().subtract(10, 'days');
+/* by default, set the lastVote to when the app is started */
+var lastVote = moment();
 
-app.post('/sms', function(req, res) {
-    if(twilio.validateRequest(config.twilio.auth_token, req.headers['x-twilio-signature'], config.twilio.endpoint, req.body)) {
-        routeMessage(req.body.From, req.body.Body);
-        var resp = new twilio.TwimlResponse();
-        res.writeHead(200, { 'Content-Type':'text/xml' });
-        res.end(resp.toString());
-    }
-    else {
+/* express handler for incoming SMS post from twilio */
+app.post(config.web.endpoint, function(req, res) {
+    /* Authenticate that this request is coming from Twilio and not spoofed */
+    if(twilio.validateRequest(config.twilio.auth_token,
+        req.headers['x-twilio-signature'], config.twilio.endpoint, req.body)) {
+            /* pass message to router */
+            routeMessage(req.body.From, req.body.Body);
+            /* respond that we got the message OK */
+            var resp = new twilio.TwimlResponse();
+            res.writeHead(200, { 'Content-Type':'text/xml' });
+            res.end(resp.toString());
+    } else { //was not twilio; don't care
         res.send('nope');
     }
-
 });
-
+/* catchall http(s) GET */
 app.get('*', function (req, res) {
     res.send('Hello World');
 });
 
+/*
+ * replaceStr
+ * replaceStr takes in a string with {{these things}} and a data object
+ * and will return a string with {{holder}} replaced by { holder: 'whats in the data object' }
+ */
 function replaceStr(str, data) {
     Object.keys(data).forEach(function (key) {
         var val = data[key];
@@ -79,17 +94,19 @@ function replaceStr(str, data) {
     });
     return str;
 }
-
+/* process an incoming ZIP code */
 function incomingZipCode(user, zipcode) {
+    /* lookup the legislators for the zipcode */
     request ( {
         url : replaceStr(sunlightLegislatorLookup, {zip: zipcode }),
         json: true
     }, function (err, resp, body) {
-        if (!err && resp.statusCode === 200) {
+        if (!err && resp.statusCode === 200) { //if everything went ok
             if (body.results && body.count > 1) {
                 /* need user response on which one to subscribe to */
                 var message = legislatorChoiceStr;
                 userChoice[user] = [];
+                /* create message and the userChoice prompt */
                 body.results.forEach(function (key, idx) {
                     message += ' ' + replaceStr(legislatorChoiceOptionStr, {
                         n : idx + 1,
@@ -105,11 +122,12 @@ function incomingZipCode(user, zipcode) {
                 subscribe(user, bioguide_id);
             }
         } else {
-
+            //err ):
+            console.log(err);
         }
     });
 }
-
+/* first step an incoming message takes, this will route to the appropriate func */
 function routeMessage(user, message) {
     if (/^\d{5}$/.test(message)) {/* zip code */
         console.log("Recived ZIP code from", user, message);
@@ -117,26 +135,26 @@ function routeMessage(user, message) {
     } else if (/^\d{1}$/.test(message)) { /* legislator selection */
         console.log("Recieved legislator choice from", user, message);
         subscribeChoice(user, message);
-    } else if (/CLEAR/i.test(message)) { /* stop message */
+    } else if (/CLEAR/i.test(message)) { /* clear message */
         unsubscribe(user);
-    } else if (/^\?$/.test(message)) {
+    } else if (/^\?$/.test(message)) { /* breakdown request */
         breakdown(user);
-    } else {
+    } else { //what the heck did they type?
         sendManual(user);
     }
 }
-
+/* sends a breakdown if one is available, then clears the breakdown cache for that user */
 function breakdown(user) {
     if (userBreakdown[user]) {
         sendMessage(user, userBreakdown[user]);
         delete userBreakdown[user];
     }
 }
-
+/* sends the manual text to a user */
 function sendManual(user) {
     sendMessage(user, usageStr);
 }
-
+/* handles a user's legislation selection by checking their userChoice cache */
 function subscribeChoice(user, message) {
     if (userChoice[user] && userChoice[user][message - 1]) {
         subscribe(user, userChoice[user][message - 1]);
@@ -145,7 +163,7 @@ function subscribeChoice(user, message) {
         sendManual(user);
     }
 }
-
+/* handles a subscription for a user and legislator, indicated by their bioguide_id */
 function subscribe(user, bioguide_id) {
     db.connect();
     db.query(subscribeUserSQL, {number : user, bid : bioguide_id}, function(err, rows) {
@@ -153,11 +171,12 @@ function subscribe(user, bioguide_id) {
             console.log(err);
     });
     db.end();
+    /* find the name of the legislator and send confirmation message */
     bidToName(bioguide_id, function(name) {
         sendMessage(user, replaceStr(legislatorChoiceConfirm, { name : name }));
     });
 }
-
+/* unsubscribes the user from any further notifications */
 function unsubscribe(user) {
     db.connect();
     db.query(unsubscribeSQL, { number: user }, function (err, rows) {
@@ -167,7 +186,7 @@ function unsubscribe(user) {
     db.close();
     sendMessage(user, unsubscribeStr);
 }
-
+/* will parse the latest votes and compile one's that are new */
 function checkForNewVotes(body) {
     /* array where new votes will be held */
     var newVotes = [];
@@ -179,8 +198,10 @@ function checkForNewVotes(body) {
     }
     console.log(newVotes.length, "new votes found.");
     if (newVotes.length > 0) {
+        /* take the new votes (may be more than one, if wasn't updated in awhile)
+         * and notify people who have subscriptions about the vote */
         notify(newVotes);
-        lastVote = moment(body.results[0].voted_at);
+        lastVote = moment(body.results[0].voted_at); //store the last vote
     }
 }
 
@@ -197,6 +218,7 @@ function notify(votes) {
         } else {
             bill_name = vote.bill.official_title;
         }
+        //generate the vote breakdown
         var breakdownMessage = replaceStr(breakdownStr, {
             ry : vote.breakdown.party.R.Yea,
             rn : vote.breakdown.party.R.Nay,
@@ -211,14 +233,15 @@ function notify(votes) {
         /* iterate over legislator votes */
         Object.keys(vote.voter_ids).forEach(function(key) {
             db.connect();
+            /* query the DB to get subscribers to that legislator */
             db.query(getSubscribersSQL, {bid : key}, function(err, rows) {
                 if (!err) {
-                    if (rows.info.numRows > 0) {
+                    if (rows.info.numRows > 0) { //if someone is subscribed
                         /* craft message for each legislator's vote */
                         var val = vote.voter_ids[key];
                         var message = val == "Not Voting" ? noVoteStr : voteStr;
 
-                        /* convert the legislator ID to Name Party-State */
+                        /* convert the legislator b-ID to Name Party-State */
                         bidToName(key, function(legislator_name) {
                             /* Form full message */
                             message = replaceStr(message, {
@@ -227,8 +250,10 @@ function notify(votes) {
                                 bill : bill_name
                             });
                             console.log("Found", rows.info.numRows, "subscription(s) for", legislator_name);
+                            /* send message to each user subscribed to that legislator */
                             rows.forEach(function (key) {
                                 sendMessage(key.number, message);
+                                /* store the breakdown so we can send it if prompted for */
                                 userBreakdown[key.number] = breakdownMessage;
                             });
                         });
@@ -238,13 +263,13 @@ function notify(votes) {
                     console.log(err);
                 }
             });
-            db.close();
-
-
+            db.close(); /* close the db connection */
         });
     }
 }
-
+/* takes a sunlight legislator object and converts to the form
+   Nickname Lastname Party-State. E.g. Jeff Flake R-AZ. If a legislator
+   does not have a nickanme, their full first name is used. */
 function sunlightLegislatorToNamePartyState(obj) {
     var firstName = obj.nickname ? obj.nickname : obj.first_name;
     var lastName = obj.last_name;
@@ -253,12 +278,15 @@ function sunlightLegislatorToNamePartyState(obj) {
     var full = name + ' ' + partyState;
     return full;
 }
-
+/* takes a bioguide-id and returns a full name, party and state
+    will pass the name as a parameter to the cb function
+*/
 function bidToName(bid, cb) {
-    if (nameCache.bid) {
+    if (nameCache.bid) { //lookup if name is cached
         cb(nameCache.bid);
         return;
     }
+    /* hit sunlight api to get info */
     request ( {
         url: replaceStr(sunlightLegislator, { bid : bid }),
         json: true
@@ -266,17 +294,17 @@ function bidToName(bid, cb) {
         if (!err && resp.statusCode === 200) {
             if (body.count > 0) {
                 var full = sunlightLegislatorToNamePartyState(body.results[0]);
-                nameCache[bid] = full;
-                cb(full);
+                nameCache[bid] = full; //store in cache
+                cb(full); //call CB with the full name party state */
             }
         } else {
             console.log(err);
         }
     });
 }
-
+/* sends a text message */
 function sendMessage(number, body) {
-    if (DEBUG) {
+    if (DEBUG) { //if debug, don't send; just log.
         console.log("SEND:", body, number);
         return;
     }
@@ -293,7 +321,7 @@ function sendMessage(number, body) {
     });
 }
 
-
+/* set interval to check for new votes! */
 setInterval(function() {
     request( {
         url : sunlightBill,
